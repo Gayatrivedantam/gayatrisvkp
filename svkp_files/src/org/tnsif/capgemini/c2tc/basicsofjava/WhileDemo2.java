507d066eebed5bceaf4bb329d3d3ee4a36da9244
LPT-07 <LPT-07@192.168.1.42>
2024-12-17 16:17:31 +0530

    commit

--- svkp_files/src/org/tnsif/capgemini/c2tc/basicsofjava/WhileDemo2.java
+++ svkp_files/src/org/tnsif/capgemini/c2tc/basicsofjava/WhileDemo2.java
@@ -1,5 +1,16 @@
 package org.tnsif.capgemini.c2tc.basicsofjava;
 
-public class WhileDemo2 {
 
-}
+	public class WhileDemo2 {
+		public static void main(String[] args) {
+			int i=1;
+			while(i<=10) //1<10
+			{
+				System.out.println("Tomorrow is holiday  "+i);
+				i++;
+			}
+
+	}
+	}
+	
+
