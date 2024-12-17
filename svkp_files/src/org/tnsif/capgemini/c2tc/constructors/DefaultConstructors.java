891e080101dbc29e446924008402f9a21dff91eb
LPT-07 <LPT-07@192.168.1.42>
2024-12-17 15:05:11 +0530

    commit2

---
+++ svkp_files/src/org/tnsif/capgemini/c2tc/constructors/DefaultConstructors.java
@@ -0,0 +1,23 @@
+package org.tnsif.capgemini.c2tc.constructors;
+class Myclass1
+{
+	String color;
+	int cost;
+	String brand;
+	
+	
+	//default constructor
+	Myclass1()
+	{
+		color="Pink";
+		cost=2000000;
+		brand="BMW";
+	}
+}
+public class DefaultConstructors {
+	public static void main(String[] args) {
+		Myclass1 obj=new Myclass1();
+		System.out.println(obj.color + " "+ obj.cost+ " " +obj.brand);
+		System.out.println(obj.color + " "+ obj.cost+ " " +obj.brand);
+	}	
+}
